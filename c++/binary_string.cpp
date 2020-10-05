6ebf05aa252084409df59eec3256e7201428fd19
Ankush Yadav <ankushyadav9302@gmail.com>
2020-10-05 19:53:01 +0530

    [REFACTOR] maintain consistency
    fixed indentation, spaces

--- c++/binary_string.cpp
+++ c++/binary_string.cpp
@@ -21,48 +21,43 @@ Generate a binary string of given length
 #include<bits/stdc++.h>
 #include<string>
 using namespace std;
-void generateStringUtil(int k,char str[],int n)
-{
-  if(n==k)
-  {
-    str[n]='\0';
-    cout<<str<<endl;
-    return;
-  }
-  if(str[n-1]=='0')
-  {
-    str[n]='0';
-    generateStringUtil(k,str,n+1);
-    str[n]='1';
-    generateStringUtil(k,str,n+1);
-  }
-  if(str[n-1]=='1')
-  {
-    str[n]='0';
-    generateStringUtil(k,str,n+1);
-  }
+
+void generateStringUtil(int k, char str[], int n) {
+    if (n == k) {
+        str[n] = '\0';
+        cout << str << endl;
+        return;
+    }
+    if (str[n-1] == '0') {
+        str[n] = '0';
+        generateStringUtil(k, str, n+1);
+        str[n] = '1';
+        generateStringUtil(k, str, n + 1);
+    }
+    if (str[n - 1] == '1') {
+        str[n] = '0';
+        generateStringUtil(k, str, n + 1);
+    }
 }
-void generateStrings(int k)
-{
-  if(k<=0)
-  {
-    return;
-  }
-  char str[k];
-  str[0]='0';
-  generateStringUtil(k,str,1);
-  str[0]='1';
-  generateStringUtil(k,str,1);
+
+void generateStrings(int k) {
+    if(k<=0) {
+        return;
+    }
+    char str[k];
+    str[0] = '0';
+    generateStringUtil(k, str, 1);
+    str[0] = '1';
+    generateStringUtil(k, str, 1);
 }
-int main()
-{
-  int t;
-  cin>>t;
-  while(t--)
-  {
-    int k;
-    cin>>k;
-    generateStrings(k);
-  }
-  return 0;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k;
+        cin >> k;
+        generateStrings(k);
+    }
+    return 0;
 }
